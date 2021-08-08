ab6a0e5621a00242c9bcf55ce6dc9f68f713c8d1
unknown <paulclue20@gmail.com>
2021-08-07 20:16:08 -0500

    Initial commit for milestone3 branch

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function BooksForm() {
+  const bk = useSelector((state) => state.addRemoveBook);
   const cat = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   return (
     <form>
