7fcbef5e9a5cbad410c11f101e46c7a63dd01dfc
unknown <paulclue20@gmail.com>
2021-08-09 11:10:27 -0500

    Update view

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import BooksForm from '../containers/BooksForm';
 function App() {
   return (
     <>
-      <div className="panel-bg"><span className="Bookstore-CMS Text-Style-5">Bookstore CMS</span></div>
       <div className="list">
         <BooksForm />
       </div>
